d64adedf0cdc2c8f2ea3689ba840fa1a80b0dfd0
Yurika Jacobsson <nakiruy@gmail.com>
2017-10-16 15:25:55 -0700

    Clean assets and fixed positioning for home component.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,17 +1,8 @@
 import React from 'react';
 import { Route, Link as Router } from 'react-router-dom';
-import '../../css/style.scss'
 
 import Navigation from './Navigation';
 import Home from './Home';
-import Description from './Description';
-import SellingPoints from './SellingPoints';
-import Video from './Video';
-import Stats from './Stats';
-import Feedback from './Feedback';
-import SocialMedia from './SocialMedia';
-import Subscribe from './Subscribe';
-import Footer from './Footer';
 
 class App extends React.Component {
   constructor(props) {
@@ -24,16 +15,6 @@ class App extends React.Component {
       <div>
           <div className="header">
             <Home />
-          </div>
-          <div className="content">
-            <Description />
-            {/* <SellingPoints />
-            <Video />
-            <Stats />
-            <Feedback />
-            <Footer />
-            <Subscribe />
-            <SocialMedia /> */}
           </div>
       </div>
     )
